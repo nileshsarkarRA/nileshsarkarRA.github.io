ed5e7ae4aa62d71c3b08d76641983fa1a23c899f
Nilesh Sarkar <nileshsarkar.cs@gmail.com>
2024-08-18 22:42:22 +0530

    Added a new theme for the portfolio v1.0 theme: elden ring

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-alert("Welcome to my website! I am Nilesh Sarkar! An AI ML Engineer")
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', smoothScroll);
+});
+
+function smoothScroll(event) {
+  event.preventDefault();
+  const targetId = this.getAttribute('href');
+  const targetElement = document.querySelector(targetId);
+  const offsetTop = targetElement.offsetTop;
+
+  window.scrollTo({
+    top: offsetTop,
+    behavior: 'smooth'
+  });
+}
+
+const headings = document.querySelectorAll('h1, h2, h3');
+
+headings.forEach(heading => {
+  const letters = heading.textContent.split('');
+  heading.textContent = '';
+
+  letters.forEach((letter, index) => {
+    const span = document.createElement('span');
+    span.textContent = letter;
+    span.style.animationDelay = `${index * 50}ms`; // Adjust delay as needed
+    heading.appendChild(span);
+  });
+});
